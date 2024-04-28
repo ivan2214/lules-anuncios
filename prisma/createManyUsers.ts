f38ce0e80bee610993049d896605651eeab0d8ec
ivan2214 <bongiovanniivan12@gmail.com>
2024-04-28 04:54:36 -0300

    add faker

--- prisma/createManyUsers.ts
+++ prisma/createManyUsers.ts
@@ -1,12 +1,20 @@
 import { faker } from "@faker-js/faker";
 import { db } from "../lib/db";
+import { UserRole } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 export const createManyUsers = async (
   numberOfUsers: number | undefined = 55
 ) => {
+  const password = "123456";
+  const hashPassword = await bcrypt.hash(password, 10);
   const users = Array.from({ length: numberOfUsers }).map(() => ({
     name: faker.person.fullName(),
     email: faker.internet.email(),
+    image: faker.image.avatar(),
+    role: faker.datatype.boolean() ? UserRole.ADMIN : UserRole.USER,
+    hashPassword,
+    emailVerified: faker.datatype.boolean() ? new Date() : null,
   }));
 
   await db.user.createMany({ data: users });
